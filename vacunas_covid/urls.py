65e10515db21e16dff4b1c9d6641fe8b78f63641
Francy <francyberliz@gmail.com>
2021-10-21 16:06:26 -0500

    Despliegue de modificacion correctamente realizada

--- vacunas_covid/urls.py
+++ vacunas_covid/urls.py
@@ -1,18 +1,20 @@
 from django.contrib                 import admin
-from django.urls                    import path
+from django.urls                    import path, include 
 from rest_framework_simplejwt.views import(TokenObtainPairView, TokenRefreshView)
 from asignacion_vacunas             import views as authAppViews
+from django.conf.urls.static        import static
+from django.conf                    import settings
 
 urlpatterns = [
-    path('admin/',                                 admin.site.url),
+    path('admin/',                                 admin.site.urls),
     path('login/',                                 TokenObtainPairView.as_view()),
     path('refresh/',                               TokenRefreshView.as_view()),    
     path('territorio/',                            authAppViews.TerritorioCreateView.as_view()),
     path('territorio/<int:pk>/',                   authAppViews.TerritorioDetailView.as_view()),
-    path('asignacion/',                            views.asignacionCreateView.as_view()),
-    path('asignacion/<int:user>/',                 views.asignacionUserView.as_view()),
-    path('asignacion/remove/<int:user>/<int:pk>/', views.asignacionDeleteView.as_view()),
-    path('asignacion/update/<int:user>/<int:pk>/', views.asignacionUpdateView.as_view()),
+    path('asignacion/',                            authAppViews.AsignacionCreateView.as_view()),
+    path('asignacion/<int:user>/',                 authAppViews.AsignacionTerritorioView.as_view()),
+    path('asignacion/remove/<int:user>/<int:pk>/', authAppViews.AsignacionDeleteView.as_view()),
+    path('asignacion/update/<int:user>/<int:pk>/', authAppViews.AsignacionUpdateView.as_view()),
 ]
 
 if settings.DEBUG:
