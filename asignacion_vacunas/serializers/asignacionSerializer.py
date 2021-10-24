9230d667932e28a3481f8e895405e2c0425fdbe0
Francy <francyberliz@gmail.com>
2021-10-24 18:53:43 -0500

    despliegue de cambios 10

--- asignacion_vacunas/serializers/asignacionSerializer.py
+++ asignacion_vacunas/serializers/asignacionSerializer.py
@@ -8,25 +8,22 @@ class AsignacionSerializer(serializers.ModelSerializer):
         fields = ['num_resolucion','fecha_resolucion', 'anio', 'laboratorio_vacuna', 'cantidad', 'uso_vacuna', 'fecha_corte'] 
 
     def to_representation(self, obj):        
-        territorio = Territorio.objects.get(cod_territorio=obj.territorio)
+        territorio = Territorio.objects.get(cod_territorio=obj.territorio_cod_territorio)
         asignacion = Asignacion.objects.get(id=obj.id)
         
         return {
+            'num_resolucion'    : asignacion.num_resolucion,   
+            'fecha_resolucion'  : asignacion.fecha_resolucion, 
+            'anio'              : asignacion.anio,             
+            'laboratorio_vacuna': asignacion.laboratorio_vacuna,
+            'cantidad'          : asignacion.cantidad,         
+            'uso_vacuna'        : asignacion.uso_vacuna,        
+            'fecha_corte'       : asignacion.fecha_corte,                            
             'territorio':{
                 'cod_territorio': territorio.cod_territorio,
                 'username'      : territorio.username,
-                'nom_territorio': territorio.nom_territorio,
-                
-            }, 'asignacion' : {
-                    'num_resolucion'    : asignacion.num_resolucion,   
-                    'fecha_resolucion'  : asignacion.fecha_resolucion, 
-                    'anio'              : asignacion.anio,             
-                    'laboratorio_vacuna': asignacion.laboratorio_vacuna,
-                    'cantidad'          : asignacion.cantidad,         
-                    'uso_vacuna'        : asignacion.uso_vacuna,        
-                    'fecha_corte'       : asignacion.fecha_corte,      
-                }       
-
+                'nom_territorio': territorio.nom_territorio                
+            }
         }
 
    
